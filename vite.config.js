889cd76c5ea113fb82a8ee1937464f0f2336a52c
Fares <fakifu67@gmail.com>
2026-08-17 14:26:02 +0200

    feat: configure iOS apple-touch-icon and PWA manifest icon tags

--- vite.config.js
+++ vite.config.js
@@ -29,6 +29,12 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
+            src: 'apple-touch-icon-180x180.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
             src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
